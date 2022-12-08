526c75938a93abdb7f30d47a36d7587b6dcf990d
Nikita1 <nikitavihrov@example.com>
2022-12-08 19:16:43 +0300

    Изменили файл main.py на веткке hotfix

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import random
 
 def password():
     s1 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' ]
+def password():
+    s1 = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','>
+    result = ''
+    for i in range(11):
+        result += random.choice(s1)
+    return result
+
+p = password()
+print(p)
     result = ''
     for i in range(11):
     	result += random.choice(s1)
@@ -9,3 +18,4 @@ def password():
 
 p = password()
 print(p)
+
